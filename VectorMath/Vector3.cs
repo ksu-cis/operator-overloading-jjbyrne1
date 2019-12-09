7a9d9b1f971e3a299d860f731d1670fbcc96f304
jjbyrne <jjbyrne@F1092-09-2097>
2019-12-09 16:17:41 -0600

    done

--- VectorMath/Vector3.cs
+++ VectorMath/Vector3.cs
@@ -17,12 +17,83 @@ namespace VectorMath
             Z = z;
         }
 
+        public override string ToString()
+        {
+            return $"<{this.X}, {this.Y}, {this.Z}>";
+        }
+
         // Vector Addition
+        public static Vector3 Add(Vector3 v1, Vector3 v2)
+        {
+            return new Vector3(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
 
-        // Vector Subtraction 
+        public Vector3 Add(Vector3 other)
+        {
+            return new Vector3(this.X + other.X, this.Y + other.Y, this.Z + other.Z);
+        }
 
-        // Vector Scaling 
+        public void AddInPlace(Vector3 other)
+        {
+            this.X += other.X;
+            this.Y += other.Y;
+            this.Z += other.Z;
+        }
+
+        public static Vector3 operator +(Vector3 v1, Vector3 v2)
+        {
+            return new Vector3(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+
+        // Vector Subtraction
+        public static Vector3 operator -(Vector3 v1, Vector3 v2)
+        {
+            return new Vector3(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        }
+
+        // Vector Scaling
+        public static Vector3 operator *(double s, Vector3 v)
+        {
+            return new Vector3(s * v.X, s * v.Y, s * v.Z);
+        }
+
+        public static Vector3 operator *(Vector3 v, double s)
+        {
+            return new Vector3(s * v.X, s * v.Y, s * v.Z);
+        }
 
         // Vector Comparison
+        public static bool operator ==(Vector3 v1, Vector3 v2)
+        {
+            return v1.X == v2.X && v1.Y == v2.Y && v1.Z == v2.Z;
+        }
+
+        public static bool operator !=(Vector3 v1, Vector3 v2)
+        {
+            return !(v1 == v2);
+        }
+
+        //Vector increment
+        public static Vector3 operator ++(Vector3 v)
+        {
+            return new Vector3(v.X++, v.Y++, v.Z++);
+        }
+
+        public static double operator ~(Vector3 v)
+        {
+            return Math.Sqrt(v.X * v.X + v.Y * v.Y + v.Z * v.Z);
+        }
+
+        public static readonly Vector3 Zero = new Vector3(0, 0, 0);
+
+        public static bool operator true(Vector3 v)
+        {
+            return Zero != v;
+        }
+
+        public static bool operator false(Vector3 v)
+        {
+            return Zero == v;
+        }
     }
 }
